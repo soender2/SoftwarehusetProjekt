2828e601b103feb5ded80fd7843359ad94f7c9a2
H3lsoe <morten@helsoe.dk>
2022-04-04 14:22:10 +0200

    EditActivity andet scenario, lavet færdig

--- features/edit_activity.feature
+++ features/edit_activity.feature
@@ -11,15 +11,14 @@ Scenario: Edit the name of an activity successfully
 	When the name is edited
 	Then the name of the activity is "testing 2"
 
-#Scenario: Edit the employee working on the activity successfully
-#	Given that the user is project manager with the initials "MOHE"
-#	And there is a project called "clean", with project number "20"
-#	And the project contains an activity with the name "vaccum"
-#	And an employee with the initials "OSMA"
-#	And an employee with the initials "RASØ"
-#	When the employee with the initials "OSMA" is replaced by the employee with the initials "RASØ"
-#	Then the activity has the employee with the initials "RASØ"
-#
+Scenario: Edit the employee working on the activity successfully
+	Given there is a project with project name "Project 1"
+	And the user with initials "name" is the manager of the project
+	And the project have an activity named "testing"
+	And an employee with the initials "name 2" is assigned to the activity
+	When the employee with the initials "name2" is replaced by the employee with the initials "name3"
+	Then the activity has the employee with the initials "name3"
+
 #Scenario: Edit the name of an activity without being project manager
 #	Given that the user with the initials "MOHE" is not manager of the project
 #	And there is a project called "clean"
--- src/test/java/dtu/calculator/EditActivity.java
+++ src/test/java/dtu/calculator/EditActivity.java
@@ -4,6 +4,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import system.app.Activity;
+import system.app.Employee;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -29,7 +31,25 @@ public class EditActivity {
 
     @Then("the name of the activity is {string}")
     public void the_name_of_the_activity_is(String string) {
-
         assertEquals(testData.activity.getName(), name);
     }
+    @Given("an employee with the initials {string} is assigned to the activity")
+    public void an_employee_with_the_initials_is_assigned_to_the_activity(String string) {
+        testData.employee = new Employee(string);
+        testData.activity.assignEmployeeActivities(testData.employee);
+    }
+
+    @When("the employee with the initials {string} is replaced by the employee with the initials {string}")
+    public void the_employee_with_the_initials_is_replaced_by_the_employee_with_the_initials(String string, String string2) {
+        testData.activity.assignEmployeeActivities(testData.employee = new Employee(string2));
+        assertTrue(string != testData.employee.getEmployeeId());
+    }
+
+    @Then("the activity has the employee with the initials {string}")
+    public void the_activity_has_the_employee_with_the_initials(String string) {
+        assertEquals(string, testData.activity.getEmployeeId());
+    }
+
+
+
 }
