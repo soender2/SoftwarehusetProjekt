56b90f7a2e26c291e9b88b11889189935afb2c0c
OscarWulff <Oscar@mandrupsen.dk>
2022-04-04 17:53:08 +0200

    availableEmployees færdig

--- src/test/java/dtu/calculator/StaffEmployee.java
+++ src/test/java/dtu/calculator/StaffEmployee.java
@@ -36,40 +36,31 @@ public class StaffEmployee {
 
     @Given("the given employee named {string} is available")
     public void the_given_employee_named_is_available(String string) {
-        Employee worker = new Employee(string);
-        testData.pma.addEmployee(worker);
+        ArrayList<Employee> availableemployees = testData.pma.availableEmployees();
 
-        assertTrue(testData.pma.isEmployeeAvailable(string));
-    }
-
-    @When("the user {string} staffs the employee {string} to the activity {string}")
-    public void the_user_staffs_the_employee_to_the_activity(String string, String string2, String string3) {
-        if(testData.project.getProjectManager() == null || !testData.project.getProjectManager().employeeId.equals(string)) {
-            testData.errorMessageHolder.setErrorMessage("Projectmanager is not registered");
-        } else {
-            Activity activity = testData.project.getActivity(string3);
-            activity.assignEmployeeActivities(testData.pma.getEmployee(string2));
-            testData.project.addActivity(activity);
+        for(Employee employee: availableemployees) {
+            if(employee.employeeId.equals(string)) {
+                assertTrue(employee.EmployeeAvailable());
+            }
         }
     }
+    @When("the user {string} staffs the employee {string} to the activity {string}")
+    public void the_user_staffs_the_employee_to_the_activity(String string, String string2, String string3) {
+        ArrayList<Employee> availableemployees = testData.pma.availableEmployees();
+        Employee worker = null;
 
-
+        for(Employee employee: availableemployees) {
+            if(employee.employeeId.equals(string2)) {
+                worker = employee;
+            }
+        }
+        assert worker != null;
+        testData.project.getActivity(string3).assignEmployeeActivities(worker);
+    }
     @Then("the employee {string} is staffed to the activity {string}")
     public void the_employee_is_staffed_to_the_activity(String string, String string2) {
+        System.out.println(testData.project.getActivity(string2).getEmployeeId());
         assertEquals(string, testData.project.getActivity(string2).getEmployeeId());
     }
 
-    @Given("the given employee named {string} is not available")
-    public void the_given_employee_named_is_not_available(String string) {
-            assertFalse(testData.pma.isEmployeeAvailable(string));
-            testData.errorMessageHolder.setErrorMessage("Employee is not available");
-    }
-
-    @Given("the activity named {string} is already staffed")
-    public void the_activity_named_is_already_staffed(String string) {
-        Employee employee = new Employee("test");
-        testData.project.getActivity(string).assignEmployeeActivities(employee);
-        assertTrue(testData.project.getActivity(string).isActivityStaffed());
-        testData.errorMessageHolder.setErrorMessage("Activity is already staffed");
-    }
 }
--- src/test/java/dtu/calculator/availableEmployee.java
+++ src/test/java/dtu/calculator/availableEmployee.java
@@ -3,8 +3,39 @@ package dtu.calculator;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import system.app.Activity;
+import system.app.Employee;
+import system.app.PMA;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class availableEmployee {
+    Employee employee;
+    TestData testData;
+    Activity activity;
 
+    public availableEmployee(TestData testData) {
+        this.testData = testData;
+        this.testData.pma = new PMA();
+        this.testData.errorMessageHolder = new ErrorMessageHolder();
+    }
+
+
+    @When("the user request to see available employees")
+    public void the_user_request_to_see_available_employees() {
+        testData.pma.availableEmployees();
+    }
+
+    @Then("there is given a list with all names of available employees")
+    public void there_is_given_a_list_with_all_names_of_available_employees() {
+        System.out.println(testData.pma.availableEmployees());
+        for (Employee employee : testData.pma.availableEmployees()) {
+                assertTrue(employee.EmployeeAvailable());
+        }
+    }
 }
+
 
