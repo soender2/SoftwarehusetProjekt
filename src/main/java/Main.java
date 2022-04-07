0ced490d89e7436db17feb3c3c145f95a4f5b442
soender2 <rasm847i@gmail.com>
2022-04-07 12:49:02 +0200

    GUI er startet

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import Controllers.SceneManager;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL url = new File("src/main/java/MainScene.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        primaryStage.setTitle("HelloWorld");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+}
