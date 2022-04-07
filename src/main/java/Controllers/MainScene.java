7b3928bbbd68666adef860f2e009d6eb7e9dd817
OscarWulff <Oscar@mandrupsen.dk>
2022-04-07 17:27:47 +0200

    DET VIRKER!!!!

--- src/main/java/Controllers/MainScene.java
+++ src/main/java/Controllers/MainScene.java
@@ -1,8 +1,38 @@
 package Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+
 public class MainScene {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    public TextField Initials;
+    public Button loginbutton;
+
+
+    public void goToSystem(ActionEvent event) throws IOException {
+        URL url = new File("src/test/resources/fxml/System.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
 }
-
 
